95850808574c06b458ad544424fe8c3209143bdc
ndavis67-hub <ndavis67@ut.utm.edu>
2026-03-04 01:24:53 +0000

    Header

---
+++ stacker.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#ifndef STACKER_H
+#define STACKER_H
+
+class Stacker {
+
+private:
+    string magic_number;
+
+    int width, height, max_color;
+
+    struct{
+        int r, g, b;
+    } pixel;
+
+    vector<int> pixel;
+
+public:
+
+
+};
+#endif
