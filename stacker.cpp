5294d5fa61dada6f78cf60b1397b4fd59d088853
JoeyC2 <123003131+JoeyC2@users.noreply.github.com>
2026-03-04 02:25:50 +0000

    basic speech

--- stacker.cpp
+++ stacker.cpp
@@ -5,7 +5,15 @@
 #include <sstream>
 using namespace std;
 int main() {
+ string name = "";
+ int count =0;
+ cout<<"Please enter the general name of the image to stack"<<endl;
+ cin>>name;
+ cout<<"please enter the amount of images to be proccessed"<<endl;
+ cin>>count;
+ 
+//processing code hear 
 
-
+cout<<"stacking images "<<count<<endl;
   return 0;
 }
