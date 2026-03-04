f6350ac146ad551c13f94a48f37b45e37d7794e9
JoeyC2 <123003131+JoeyC2@users.noreply.github.com>
2026-03-04 01:23:36 +0000

    thoughtsonstakcerfucntioninheritence

--- main.cpp
+++ main.cpp
@@ -7,7 +7,18 @@
 using namespace std;
 //hello world
 int main() {
+
+  //int file 
+  //int file2
+  //int file3 
+  //funciton(file1, file2 and so on )
+  //alt stacker does this in stacker.cpp 
+
+  // probibkly dont need function called with 10 varibles input here  
+  //function output 
+  //put fileinputs hear for muliple files 
   
-  
+  //output total of the  photos as single output 
+
   return 0;
 }
