2802b56acc698a4bedab6fede29578f6bac3f009
JoeyC2 <123003131+JoeyC2@users.noreply.github.com>
2026-03-06 22:46:44 +0000

    makingvectors

--- main.cpp
+++ main.cpp
@@ -7,18 +7,38 @@
 using namespace std;
 //hello world
 int main() {
+  int i =0, j=0, k=0, l=0;
+  int count =0;
+  for(count )
+  vector<vector<vector<vector<int>>>> vector1(R, vector<vector<vector<int>>>(G, vector<vector<int>>(B, vector<int>(3,0) )));
+  vector1[i][j][k][l]
+//i = Red 
+//j = Green
+//k = Blue
+//l pixel number 
+cout<<"enter file name"<<endl;
+string filename ="";
+cin>>filename;
+fstream file(filename);
+// dont forget 
+int f =0;
+int o =0;
+int u =0;
+while(file>>f>>o>>u){
+  vector1[i][j][k][l] = f;
+  i++;
+  vector1[i][j][k][l] = o;
+  j++;
+  vector1[i][j][k][l] = u;
+  k++;
+  l++;
+}
+i=0;
+j=0
+k=0;
+l=0;
 
-  //int file 
-  //int file2
-  //int file3 
-  //funciton(file1, file2 and so on )
-  //alt stacker does this in stacker.cpp 
 
-  // probibkly dont need function called with 10 varibles input here  
-  //function output 
-  //put fileinputs hear for muliple files 
-  
-  //output total of the  photos as single output 
 
   return 0;
 }
