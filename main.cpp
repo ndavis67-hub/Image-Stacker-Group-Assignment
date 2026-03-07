893f17c2e701653dc23300da7827f1640acf8561
JoeyC2 <123003131+JoeyC2@users.noreply.github.com>
2026-03-07 00:07:49 +0000

    this thing

--- main.cpp
+++ main.cpp
@@ -7,13 +7,12 @@
 using namespace std;
 //hello world
 int main() {
-  int i =0, j=0, k=0;
+  int i =0;
+  int f =0;
+  int o =0;
+  int u =0;
   int count =0;
  vector<vector<vector<int>>> vector1 ;
- vector1[i] = {f, o, u };
-//i = Red 
-//j = Green
-//k = Blue
 //l pixel number 
 cout<<"enter file name"<<endl;
 string filename ="";
@@ -23,16 +22,14 @@ while(count < 10){
 
   fstream file(filename + "00"+ count +".ppm");
 // dont forget 
-  int f =0;
-  int o =0;
-  int u =0;
+ 
   
 //run something to remove the first 1 ,2 ,3 line of file input ie P3 591 600 255
   while(file>>f>>o>>u){
-    vector1[i] = {f, o, u };
-    i++
+    vector1.push_back({{f, o, u }});
+    i++;
   }
-    file.close()
+    file.close();
    
     count++;
 }
