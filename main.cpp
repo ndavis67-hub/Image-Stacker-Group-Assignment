fdd33e9ef7dd316aa9f32fe4e139435c58fdd482
JoeyC2 <123003131+JoeyC2@users.noreply.github.com>
2026-03-07 00:36:36 +0000

    badmath

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 using namespace std;
 //hello world
 int main() {
-  int i =0;
   int f =0;
   int o =0;
   int u =0;
   int count =0;
+  int l =0;
  vector<vector<vector<int>>> vector1 ;
 //l pixel number 
 cout<<"enter file name"<<endl;
@@ -22,19 +22,38 @@ while(count < 10){
 
   fstream file(filename + "00"+ count +".ppm");
 // dont forget 
- 
+  string garbage;
+  getline(file, garbage);
+  getline(file, garbage);
+  getline(file, garbage);
+
   
 //run something to remove the first 1 ,2 ,3 line of file input ie P3 591 600 255
   while(file>>f>>o>>u){
-    vector1.push_back({{f, o, u }});
-    i++;
-  }
+    if(count == 0){
+      vector1.push_back({{f, o, u }});
+    }
+    else {
+      vector1[l][0] +=f;
+      vector1[l][1] +=o;
+      vector1[l][2] +=u;
+      l++;
+      
+    }
     file.close();
-   
     count++;
+    
 }
-//end of loop 
+//end of loop
+for(int a = 0; a> vector1.size() a++){
+ vector1 =[a][0] /=10;
+ vector1 =[a][1] /=10;
+ vector1 =[a][2] /=10;
+ 
+}
+ 
 
 
-  return 0;
+    return 0;
+  }
 }
